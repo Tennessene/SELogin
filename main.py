15c55243104bdfd77b4aec4941958c466c214242
Anston Sorensen <ansorensen1118@gmail.com>
2024-08-15 22:19:09 -0500

    Fix debugging port

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     chrome_options.add_argument("--headless")  # Run Chrome in headless mode
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--remote-debugging-port=0")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     # Path to ChromeDriver
     service = Service("chromedriver-linux64/chromedriver")  # Adjust the path as needed
