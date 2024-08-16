ac4af6812c827f532df9a728661b18317ea2108f
Anston Sorensen <ansorensen1118@gmail.com>
2024-08-15 19:35:13 -0500

    Fix string syntax inconsistency

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def visit_url(url, sleep_time):
     Navigates to a given url.
 
     Args:
-        url (str): The url of the site to visit (e.g., 'https://stackexchange.com/').
+        url (str): The url of the site to visit (e.g., "https://stackexchange.com/").
         sleep_time (int): The time in seconds to sleep before moving on.
     """
-    print(f'Visiting {url}')
+    print(f"Visiting {url}")
     driver.get(url)
     time.sleep(sleep_time) # Wait for the page to load
 
@@ -26,16 +26,16 @@ def visit_site(name):
     Args:
         name (str): The name of the site to visit (e.g., 'superuser').
     """
-    url = f'https://{name}.com/'
+    url = f"https://{name}.com/"
     visit_url(url, 2)
 
     # Visit meta site
-    if ".stackexchange" in name:
+    if '.stackexchange' in name:
         namearray = name.split('.')
         basename = namearray[0]
-        url = f'https://{basename}.meta.stackexchange.com/'
+        url = f"https://{basename}.meta.stackexchange.com/"
     else:
-        url = f'https://meta.{name}.com/'
+        url = f"https://meta.{name}.com/"
 
     visit_url(url, 2)
 
@@ -45,23 +45,23 @@ def login_stackexchange():
     # Logs into Stack Exchange using the provided credentials.
 
     name = 'stackexchange'
-    url = f'https://{name}.com/users/login'
+    url = f"https://{name}.com/users/login"
     visit_url(url, 0)
 
     wait = WebDriverWait(driver, 10)
 
-    email = wait.until(EC.element_to_be_clickable((By.ID, "email")))
-    password = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    email = wait.until(EC.element_to_be_clickable((By.ID, 'email')))
+    password = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
 
     # Enter your credentials
-    email.send_keys('${{ secrets.EMAIL }}')
-    password.send_keys('${{ secrets.PASSWORD }}')
+    email.send_keys("${{ secrets.EMAIL }}")
+    password.send_keys("${{ secrets.PASSWORD }}")
     password.send_keys(Keys.RETURN)
 
     time.sleep(3)  # Wait for login to complete
 
     # Visit meta site
-    url = f'https://meta.{name}.com/'
+    url = f"https://meta.{name}.com/"
     visit_url(url, 2)
 
 
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     chrome_options.add_argument("--remote-debugging-port=0")
 
     # Path to ChromeDriver
-    service = Service('chromedriver-linux64/chromedriver')  # Adjust the path as needed
+    service = Service("chromedriver-linux64/chromedriver")  # Adjust the path as needed
 
     # Initialize the WebDriver
     driver = webdriver.Chrome(service=service, options=chrome_options)
